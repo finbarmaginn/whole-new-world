d5b8b50ce4c39cb38dfc4f08fc23cc3810e3fe1b
finbarmaginn <finbarmaginn@gmail.com>
2017-12-11 16:05:21 +0000

    npm start, use node not nodemon

--- src/app/browser.js
+++ src/app/browser.js
@@ -1,12 +1,15 @@
+// include polyfills
 import 'babel-polyfill'
 import './polyfill'
 
+// create react app
 import React from 'react'
 import {hydrate} from 'react-dom'
 import App from './index'
 import Promise from 'promise-polyfill'
 import {Provider} from 'react-redux'
 import store from './reducers'
+
 if (!window.Promise) {
   window.Promise = Promise
 }
