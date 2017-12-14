d84d4b987d97416b0c0919121c1a1238e46842eb
finbarmaginn <finbarmaginn@gmail.com>
2017-12-14 13:01:41 +0000

    more refactored code and now some better styles

--- src/app/components/Nav/index.js
+++ src/app/components/Nav/index.js
@@ -5,20 +5,15 @@ import {
 
 if (process.env.BROWSER) require('./nav.scss')
 
-
 class Nav extends React.Component {
   render() {
-    return ( <
-      nav >
-      <
-      ul >
-      <
-      li > < Link to = "/gallery" > Gallery < /Link></li >
-      <
-      li > < Link to = "/about" > About < /Link></li >
-      <
-      /ul> < /
-      nav >
+    return (
+      <nav>
+        <ul>
+          <li><Link to="/gallery">Gallery</Link></li>
+          <li><Link to="/about">About</Link></li>
+        </ul>
+      </nav>
     )
   }
 }
